e1be614b367eef94b594f9b10032a50d64afc03c
Wolfgang Karall <office@karall-edv.at>
2015-01-30 16:19:40 +0100

    support :, & and \! notations; to support this special treatment of ':' is needed

--- ansible-completion.bash
+++ ansible-completion.bash
@@ -24,6 +24,8 @@ complete -o default -F _ansible ansible
 # the command line the completion will use it
 _ansible_complete_host() {
     local current_word=$1
+    local first_words=${current_word%:*}
+    local last_word=${current_word##*:}
     local inventory_file=$(_ansible_get_inventory_file)
     local grep_opts="-o"
     if [ -z "$inventory_file" ] && [ -f ansible.cfg ]; then
@@ -33,8 +35,13 @@ _ansible_complete_host() {
     [ -d "$inventory_file" ] && grep_opts="$grep_opts -hR"
     local hosts=$(ansible ${inventory_file:+-i "$inventory_file"} all --list-hosts 2>&1 && \
         [ -e "$inventory_file" ] && grep $grep_opts '\[.*\]' "$inventory_file" | tr -d [])
+    hosts="$hosts 
+    $(echo "$hosts" | sed -e 's/\([^[:space:]]\)/\&\1/p' -e 's/&/!/p' -e 's/!/"/p' )"
 
-    COMPREPLY=( $( compgen -W "$hosts" -- "$current_word" ) )
+    COMPREPLY=( $( compgen -W "$hosts" -- "$last_word" ) )
+    if [ "$first_words" != "$last_word" ]; then
+        COMPREPLY="$first_words:$COMPREPLY"
+    fi
 }
 
 # Look inside COMP_WORDS to find a value for the inventory-file argument
