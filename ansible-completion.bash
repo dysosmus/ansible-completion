21f78bdd61e0f5b2d23c231e4f81162b3720b278
Konstantin Manna <Konstantin@Manna.uno>
2015-07-29 14:50:35 +0200

    code cleanup (fixing recommendations by shellcheck.net)

--- ansible-completion.bash
+++ ansible-completion.bash
@@ -26,23 +26,25 @@ _ansible_complete_host() {
     local current_word=$1
     local first_words=${current_word%:*}
     local last_word=${current_word##*:}
-    local inventory_file=$(_ansible_get_inventory_file)
+    local inventory_file
     local grep_opts="-o"
+    inventory_file=$(_ansible_get_inventory_file)
     # if $inventory_file is empty and a ansible.cfg file exisit
     # search in the ansible.cfg for a hostfile entry
-    if [ -z "$inventory_file" ]; then
-        [ -f /etc/ansible/ansible.cfg ] && inventory_file=$(awk \
+    if [[ -z "$inventory_file" ]]; then
+        [[ -f /etc/ansible/ansible.cfg ]] && inventory_file=$(awk \
             '/^inventory/{ print $3 }' /etc/ansible/ansible.cfg)
-        [ -f ${HOME}/.ansible.cfg ] && inventory_file=$(awk \
-            '/^inventory/{ print $3 }' ${HOME}/.ansible.cfg)
-        [ -f ansible.cfg ] && inventory_file=$(awk \
+        [[ -f "${HOME}/.ansible.cfg" ]] && inventory_file=$(awk \
+            '/^inventory/{ print $3 }' "${HOME}/.ansible.cfg")
+        [[ -f ansible.cfg ]] && inventory_file=$(awk \
             '/^(hostfile|inventory)/{ print $3 }' ansible.cfg)
     fi
     # if inventory_file points to a directory, search recursively
-    [ -d "$inventory_file" ] && grep_opts="$grep_opts -hR"
-    local hosts=$(ansible ${inventory_file:+-i "$inventory_file"} all --list-hosts 2>&1 \
-        && [ -e "$inventory_file" ] \
-        && [ -d "$inventory_file" -o ! -x "$inventory_file" ] \
+    [[ -d "$inventory_file" ]] && grep_opts="$grep_opts -hR"
+    local hosts
+    hosts=$(ansible ${inventory_file:+-i "$inventory_file"} all --list-hosts 2>&1 \
+        && [[ -e "$inventory_file" ]] \
+        && [[ -d "$inventory_file" ]] || [[ ! -x "$inventory_file" ]] \
         && grep $grep_opts '\[.*\]' "$inventory_file" | tr -d [] | cut -d: -f1)
 
     # list the hostnames with ansible command line and complete the list
@@ -51,7 +53,7 @@ _ansible_complete_host() {
     $(echo "$hosts" | sed -e 's/\([^[:space:]]\)/\&\1/p' -e 's/&/!/p' )"
     # add the !, & notation to the hostname
 
-    if [ "$first_words" != "$last_word" ]; then
+    if [[ "$first_words" != "$last_word" ]]; then
         COMPREPLY=( $( compgen -P "$first_words:" -W "$hosts" -- "$last_word" ) )
     else
         COMPREPLY=( $( compgen -W "$hosts" -- "$last_word" ) )
@@ -63,11 +65,11 @@ _ansible_complete_host() {
 _ansible_get_inventory_file() { # @todo refactor with _ansible_get_module_path
     local index=0
 
-    for word in ${COMP_WORDS[@]}; do
-        index=$(expr $index + 1)
-        if [ "$word" != "${COMP_WORDS[COMP_CWORD]}" ]; then
+    for word in ${COMP_WORDS[*]}; do
+        ((index++))
+        if [[ "$word" != "${COMP_WORDS[COMP_CWORD]}" ]]; then
             if [[ "$word" == "-i" ]] || [[ "$word" == "--inventory-file" ]]; then
-                echo ${COMP_WORDS[$index]}
+                echo "${COMP_WORDS[$index]}"
                 return 0
             fi
         fi
@@ -80,11 +82,11 @@ _ansible_get_inventory_file() { # @todo refactor with _ansible_get_module_path
 _ansible_get_module_path() { # @todo @see _ansible_get_inventory_file
     local index=0
 
-    for word in ${COMP_WORDS[@]}; do
-        index=$(expr $index + 1)
-        if [ "$word" != "${COMP_WORDS[COMP_CWORD]}" ]; then
+    for word in ${COMP_WORDS[*]}; do
+        ((index++))
+        if [[ "$word" != "${COMP_WORDS[COMP_CWORD]}" ]]; then
             if [[ "$word" == "-M" ]] || [[ "$word" == "--module-path" ]]; then
-                echo ${COMP_WORDS[$index]}
+                echo "${COMP_WORDS[$index]}"
                 return 0
             fi
         fi
@@ -110,55 +112,58 @@ _ansible_complete_options() {
 }
 
 _ansible_get_module_list() {
-    local module_path=$(_ansible_get_module_path)
-    local hash_module_path=$(_md5 "$module_path")
+    local module_path
+    local hash_module_path
+    module_path=$(_ansible_get_module_path)
+    hash_module_path=$(_md5 "$module_path")
     # /tmp/<pid>.<hash of the module path if exsist>.module-name.ansible.completion
     local cache_file=/tmp/${$}.${module_path:+"$hash_module_path".}module-name.ansible.completion
 
-    if [ -f "$cache_file" ]; then
-        local timestamp=$(expr $(_timestamp) - $(_timestamp_last_modified $cache_file))
-        if [ "$timestamp" -gt "$ANSIBLE_COMPLETION_CACHE_TIMEOUT" ]; then
-            rm -f $cache_file > /dev/null 2>&1
-            _generate_module_cache $cache_file $module_path
+    if [[ -f "$cache_file" ]]; then
+        local timestamp=$(($(_timestamp) - $(_timestamp_last_modified "$cache_file")))
+        if [[ "$timestamp" -gt "$ANSIBLE_COMPLETION_CACHE_TIMEOUT" ]]; then
+            rm -f "$cache_file" &> /dev/null
+            _generate_module_cache "$cache_file" "$module_path"
         fi
     else
         # We need to cache the output because ansible-doc is so fucking slow
-        _generate_module_cache $cache_file $module_path
+        _generate_module_cache "$cache_file" "$module_path"
     fi
 
-    echo $(cat $cache_file)
+    cat "$cache_file"
 }
 
 _generate_module_cache() {
     local cache_file=$1
     local module_path=$2
 
-    ansible-doc ${module_path:+-M "$module_path"} -l | awk '{print $1}' > $cache_file
+    ansible-doc ${module_path:+-M "$module_path"} -l | awk '{print $1}' > "$cache_file"
 }
 
 _ansible_complete_option_module_name() {
     local current_word=$1
-    local module_list=$(_ansible_get_module_list)
+    local module_list
+    module_list=$(_ansible_get_module_list)
 
     COMPREPLY=( $( compgen -W "$module_list" -- "$current_word" ) )
 }
 
 _timestamp() {
-    echo $(date +%s)
+    date +%s
 }
 
-_timestamp_last_modified()  {
+_timestamp_last_modified() {
     local timestamp=''
 
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         # linux
-        timestamp=$(stat -c "%Z" $1)
+        timestamp=$(stat -c "%Z" "$1")
     else
         # freebsd/darwin
-        timestamp=$(stat -f "%Sm" -t "%s" $1)
+        timestamp=$(stat -f "%Sm" -t "%s" "$1")
     fi
 
-    echo $timestamp
+    echo "$timestamp"
 }
 
 _md5() {
@@ -170,8 +175,8 @@ _md5() {
         md5_hash=$(md5 -q -s "$to_hash")
     else
         # linux
-        md5_hash=$(echo "$to_hash" | md5sum |awk {'print $1'})
+        md5_hash=$(echo "$to_hash" | md5sum | awk '{print $1}')
     fi
 
-    echo $md5_hash
+    echo "$md5_hash"
 }
