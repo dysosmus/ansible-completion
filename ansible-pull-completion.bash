23ba466d10f789d0a69ed9bddf0f3b654b412231
Konstantin Manna <Konstantin@Manna.uno>
2015-07-29 18:55:18 +0200

    added ansible-pull completion

---
+++ ansible-pull-completion.bash
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+_ansible-pull() {
+    local current_word=${COMP_WORDS[COMP_CWORD]}
+    local options="--accept-host-key -K --ask-sudo-pass -C --checkout
+                   -d --directory -e --extra-vars -f --force -h --help
+                   -i --inventory-file --key-file -m --module-name -o
+                   --only-if-changed --purge -s --sleep -t --tags -U
+                   --url --vault-password-file -v --verbose"
+
+    if [[ "$current_word" == -* ]]; then
+        COMPREPLY=( $( compgen -W "$options" -- "$current_word" ) )
+    fi
+}
+
+complete -o default -F _ansible-pull ansible-pull
